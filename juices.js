680d3c0c07817f042a8abaf2e0368f46cbcaa6d9
ATasteofJade <craig.sacha@gmail.com>
2026-09-04 15:39:00 -0500

    Update juices.js

--- juices.js
+++ juices.js
@@ -155,24 +155,31 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // ==========================================
     // MINIMUM ORDER DATE
+    // CUSTOMERS CAN CHOOSE TODAY OR LATER
     // ==========================================
 
     if (orderDate) {
 
-        const fiveDaysAhead =
+        const today =
             new Date();
 
 
-        fiveDaysAhead.setDate(
-            fiveDaysAhead.getDate() +
-            5
-        );
+        const year =
+            today.getFullYear();
+
+        const month =
+            String(
+                today.getMonth() + 1
+            ).padStart(2, "0");
+
+        const day =
+            String(
+                today.getDate()
+            ).padStart(2, "0");
 
 
         orderDate.min =
-            fiveDaysAhead
-                .toISOString()
-                .split("T")[0];
+            `${year}-${month}-${day}`;
 
     }
 
